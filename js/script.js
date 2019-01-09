fb622c1849011545b5079823cc281873edd73dad
Bulat <wegwegwegw99@mail.ru>
2019-01-09 15:17:55 +0400

    refactoring script

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
+// Slider
+
 const buttons1arr = Array.prototype.slice.call(document.querySelectorAll(".slider_navigation ul li:nth-child(1) button"));
 const buttons2arr = Array.prototype.slice.call(document.querySelectorAll(".slider_navigation ul li:nth-child(2) button"));
 const buttons3arr = Array.prototype.slice.call(document.querySelectorAll(".slider_navigation ul li:nth-child(3) button"));
-
-let buttonsList = [buttons1arr, buttons2arr, buttons3arr];
-
+const buttonsList = [buttons1arr, buttons2arr, buttons3arr];
 const slideList = document.querySelectorAll(".slide_item");
-const slide1 = document.querySelector(".slide_item:nth-child(1)");
-const slide2 = document.querySelector(".slide_item:nth-child(2)");
-const slide3 = document.querySelector(".slide_item:nth-child(3)");
+
+for(let buttonsArr of buttonsList) {
+    addEventList(buttonsArr);
+}
 
 function addEventList(arr) {
     for(let button of arr) {
@@ -15,18 +16,12 @@ function addEventList(arr) {
     }
 }
 
-addEventList(buttons1arr);
-addEventList(buttons2arr);
-addEventList(buttons3arr);
-
 function showSlide(button) {    
     for(let i = 0; i < slideList.length; i++) {
         slideList[i].classList.add("displayHidden");
         if(buttonsList[i].includes(button)) {
             slideList[i].classList.remove("displayHidden");
         } 
-    }
-
-    
+    }  
 }
 
