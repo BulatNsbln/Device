1d2651218569a3513af77853157ba250f600e50a
Bulat <wegwegwegw99@mail.ru>
2019-01-11 13:42:03 +0400

    change

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
-import Slider from './Slider.js'
+class Slider {
+    constructor(buttons, slideList) {
+        this._buttonsArr = buttons;
+        this._slideList = slideList;
+        this.addClick();
+    }
+    addClick() { 
+        for(let button of this._buttonsArr) {
+            this.addEventList(button);
+        }
+    }
+    addEventList(button) {
+        button.addEventListener("click", () => this.showSlide(button));
+    }
+    showSlide(button) {   
+        for(let i = 0; i < this._slideList.length; i++) {
+            this._slideList[i].classList.add("displayHidden");
+            if(this._buttonsArr[i] == button) {
+                this._slideList[i].classList.remove("displayHidden");
+            } 
+        }  
+    }
+}
 
 // slider top
 
